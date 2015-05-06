d33930153b4ee7ecc29482945f40584ec7baab6b
pablo <eduardwildknaap@gmail.com>
2015-05-06 16:06:14 +0200

    With working custom path!

--- iapscrape.py
+++ iapscrape.py
@@ -3,9 +3,6 @@
 # Keeps your IAP up to date
 # Version 0.1
 
-dstdir = '/home/pablo/Downloads/test/'
-
-
 import os
 import sys
 import urllib2
@@ -14,8 +11,10 @@ import os.path
 import urlparse
 import posixpath
 
+
 url001 = 'http://airnav.com/airport/'
 url002 = 'http://www.ais-netherlands.nl/aim/2015-03-19-AIRAC/eAIP/html/eAIP/EH-AD-2.EHAM-en-GB.html#eham-ad-2.24/'
+
 
 def error():
         print ''
@@ -33,21 +32,32 @@ def exit():
 	return
 
 
+def dst_dir():
+	global dstdir
+	print ''
+        print 'Welcome to IAPscrape'
+	print ''
+	dstdir = raw_input('Enter destination directory: ')
+	print '' 
+	
+	airport_icao()
+
+	return
+
+
 def iap_K():
 	response = urllib2.urlopen(''+url001+''+airport+'').read()
 	charts = re.findall('href=[\'"]?([^\'" >]+).PDF',response)
 
 	for pdf in charts:
 		pdfs = 'http://airnav.com'+pdf+'.PDF'
-		#print pdfs.strip( 'http://airnav.com/depart?' )
 		cleanurl = pdfs.strip('http://airnav.com/depart?')
 		fullurl = 'http://'+cleanurl+''		
-		print fullurl
 		path = urlparse.urlsplit(fullurl).path
 		filename = posixpath.basename(path)
-		print filename
+		print 'Downloading:', filename
 		response = urllib2.urlopen(fullurl)			
-		output = open(''+dstdir+'/'+filename+'','wb')
+		output = open(''+fullpath+'/'+filename+'','wb')
 		output.write(response.read())
 		output.close()
 				
@@ -56,25 +66,23 @@ def iap_K():
 
 def airport_icao():
 	global airport
-	print ''
-	print 'Welcome to IAPscrape'
+	global fullpath
 	print ''
 	airport = raw_input('Enter the four letter ICAO designator of the aiport: ')
+
+	fullpath = ''+dstdir+'/'+airport+''
+
+	if not os.path.exists(fullpath):
+                os.makedirs(fullpath)
+
+	print ''
 
 	iap_K()
 	
 	return
 
 
-#class aip(scrapy.Charts):
-#	url = scrapy.Field()
-	
-
-
-
-
-
-airport_icao()
+dst_dir()
 
 
 # EOF
